feb9df5b1ba02bc8decbedb1166a9b9fc1fd6218
John O'shea <johncyoshea@me.com>
2021-09-22 23:13:42 -0400

    bug fix

--- script.js
+++ script.js
@@ -3,24 +3,23 @@ const p2Button = document.querySelector('#p2Button');
 const display1 = document.querySelector('#display1');
 const display2 = document.querySelector('#display2');
 const resetBtn = document.querySelector('#reset');
-const guide = document.querySelector('#guide')
-const scoreSlider = document.querySelector('#scoreSlider')
-const maxScoreDisplay = document.querySelector('#maxScoreDisplay')
+const scoreSlider = document.querySelector('#scoreSlider');
+const maxScoreDisplay = document.querySelector('#maxScoreDisplay');
 
 p1Button.disabled = true
 p2Button.disabled = true
 let winningScore = 0
 let p1Score = 0;
 let p2Score = 0;
-let GameOver = false;
+let gameOver = false;
 
 
 p1Button.addEventListener('click', function (e) {
-    if (!GameOver) {
+    if (!gameOver) {
         p1Score += 1
         display1.textContent = p1Score
         if (p1Score === winningScore) {
-            GameOver = true;
+            gameOver = true;
             display1.classList.add('text-success')
             display2.classList.add('text-danger')
             p1Button.disabled = true
@@ -31,11 +30,11 @@ p1Button.addEventListener('click', function (e) {
 })
 
 p2Button.addEventListener('click', function (e) {
-    if (!GameOver) {
+    if (!gameOver) {
         p2Score += 1
         display2.textContent = p2Score
         if (p2Score === winningScore) {
-            GameOver = true;
+            gameOver = true;
             display2.classList.add('text-success')
             display1.classList.add('text-danger')
             p1Button.disabled = true
@@ -49,21 +48,24 @@ resetBtn.addEventListener('click', function (e) {
     p2Score = 0
     scoreSlider.value = 8
     winningScore = 0
-    guide.textContent = 'set max score'
-    maxScoreDisplay.textContent = 'Score'
+    maxScoreDisplay.textContent = 'Set Max Score Below'
     display1.textContent = p1Score
     display2.textContent = p2Score
     display2.classList.remove('text-success', 'text-danger')
     display1.classList.remove('text-success', 'text-danger')
-    GameOver = false
+    gameOver = false
 
 })
 
 scoreSlider.addEventListener('change', function (e) {
-    guide.textContent = ''
-    maxScoreDisplay.textContent = scoreSlider.value
+    maxScoreDisplay.textContent = `Play To ${scoreSlider.value}`
     winningScore = parseInt(scoreSlider.value)
+    if (!gameOver) {
     p1Button.disabled = false
     p2Button.disabled = false
+} else {
+    p1Button.disabled = true
+    p2Button.disabled = true
+}
 })
 
